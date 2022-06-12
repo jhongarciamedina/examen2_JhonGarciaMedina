726f880521b649bfb8fa8a6093dd4b14a87f94d7
jhongarciamedina <jhonfarru.game@gmail.com>
2022-06-11 21:35:52 -0500

    subiendo cambios

---
+++ src/main/java/com/idat/EC2JhonGarciaMedina/Ec2JhonGarciaMedinaApplication.java
@@ -0,0 +1,13 @@
+package com.idat.EC2JhonGarciaMedina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec2JhonGarciaMedinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ec2JhonGarciaMedinaApplication.class, args);
+	}
+
+}
